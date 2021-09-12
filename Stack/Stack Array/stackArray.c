d352116372beecc182d4b5b1a9963ca13d9d31a1
Marcos Camargo <marcoscamargo@outlook.com>
2021-09-12 19:35:21 -0300

    Stack Array implemented

---
+++ Stack/Stack Array/stackArray.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+// Defining a constant
+#define MAX 10
+
+int count = 0;
+
+// Creating a stack using struct
+typedef struct structstack{
+    int items[MAX];
+    int top;
+}stack;
+
+// Initializing an empty stack 
+
+void creatEmptyStack(stack *stack){
+    stack->top = -1;
+}
+
+// Check if the stack is full
+bool isFull(stack *stack){
+    if(stack->top == MAX -1){
+        return true;
+    }
+    return false;
+}
+
+// Check if the stack is empty
+int isEmpty(stack *stack){
+    if(stack->top == -1){
+        return true;
+    }
+    return false;
+}
+
+// Add elements into stack
+bool push(stack *stack, int newItem){
+    if(isFull(stack)){
+      printf("Stack is Full!\n");
+      return false;  
+    } 
+    stack->top++;
+    stack->items[stack->top] = newItem;
+    count++;
+
+}
+
+// Remove elemnets from stack
+bool pop(stack *stack){
+    if(isEmpty(stack)){
+        printf("Stack is Empty!\n");
+        return false;
+    } 
+    stack->items[stack->top];
+    stack->top--;
+    count--;
+}
+
+//Print elements of a stack
+void printStack(stack *stack){
+    if(!isEmpty(stack)){
+        printf("Stack: ");
+        for (int i = 0; i < count; i++)
+        {
+            printf("%d ", stack->items[i]);
+        }
+        printf("\n");
+     
+    }
+}
+
+// Main Program
+int main(){
+    int ch;
+    stack *s = (stack*)malloc(sizeof(stack));
+    creatEmptyStack(s);
+    push(s, 1);
+    push(s, 2);
+    push(s, 3);
+    push(s, 4);
+    push(s, 5);
+  
+    
+    pop(s);
+    
+    printStack(s);
+    
+}
